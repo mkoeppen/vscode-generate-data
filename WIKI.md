055c8f2fedb19d29c78908b2cb6cf670f8c9f9a9
Markus Koeppen <mkoeppen@posteo.de>
2019-08-20 20:47:39 +0200

    first commands

--- WIKI.md
+++ WIKI.md
@@ -11,3 +11,18 @@ Use Generator to create the base extension
 ```
 yo code
 ```
+
+# 3. Publish Extension
+
+Install VSCE
+```
+npm install -g vsce
+```
+
+
+```
+
+```
+vsce create-publisher {{username}}
+
+https://code.visualstudio.com/api/working-with-extensions/publishing-extension#create-a-publisher
--- extension.js
+++ extension.js
@@ -1,32 +1,229 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const faker = require('faker');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const fakerGenerators = [
+	'address.zipCode',
+	'address.city',
+	'address.cityPrefix',
+	'address.citySuffix',
+	'address.streetName',
+	'address.streetAddress',
+	'address.streetSuffix',
+	'address.streetPrefix',
+	'address.secondaryAddress',
+	'address.county',
+	'address.country',
+	'address.countryCode',
+	'address.state',
+	'address.stateAbbr',
+	'address.latitude',
+	'address.longitude',
+	'commerce.color',
+	'commerce.department',
+	'commerce.productName',
+	'commerce.price',
+	'commerce.productAdjective',
+	'commerce.productMaterial',
+	'commerce.product',
+	'company.suffixes',
+	'company.companyName',
+	'company.companySuffix',
+	'company.catchPhrase',
+	'company.bs',
+	'company.catchPhraseAdjective',
+	'company.catchPhraseDescriptor',
+	'company.catchPhraseNoun',
+	'company.bsAdjective',
+	'company.bsBuzz',
+	'company.bsNoun',
+	'database.column',
+	'database.type',
+	'database.collation',
+	'database.engine',
+	'date.past',
+	'date.future',
+	'date.between',
+	'date.recent',
+	'date.month',
+	'date.weekday',
+	'finance.account',
+	'finance.accountName',
+	'finance.mask',
+	'finance.amount',
+	'finance.transactionType',
+	'finance.currencyCode',
+	'finance.currencyName',
+	'finance.currencySymbol',
+	'finance.bitcoinAddress',
+	'finance.iban',
+	'finance.bic',
+	'hacker.abbreviation',
+	'hacker.adjective',
+	'hacker.noun',
+	'hacker.verb',
+	'hacker.ingverb',
+	'hacker.phrase',
+	// 'helpers.randomize',
+	// 'helpers.slugify',
+	// 'helpers.replaceSymbolWithNumber',
+	// 'helpers.replaceSymbols',
+	// 'helpers.shuffle',
+	// 'helpers.mustache',
+	// 'helpers.createCard',
+	// 'helpers.contextualCard',
+	// 'helpers.userCard',
+	// 'helpers.createTransaction',
+	'image.image',
+	'image.avatar',
+	'image.imageUrl',
+	'image.abstract',
+	'image.animals',
+	'image.business',
+	'image.cats',
+	'image.city',
+	'image.food',
+	'image.nightlife',
+	'image.fashion',
+	'image.people',
+	'image.nature',
+	'image.sports',
+	'image.technics',
+	'image.transport',
+	'image.dataUri',
+	'internet.avatar',
+	'internet.email',
+	'internet.exampleEmail',
+	'internet.userName',
+	'internet.protocol',
+	'internet.url',
+	'internet.domainName',
+	'internet.domainSuffix',
+	'internet.domainWord',
+	'internet.ip',
+	'internet.ipv6',
+	'internet.userAgent',
+	'internet.color',
+	'internet.mac',
+	'internet.password',
+	'lorem.word',
+	'lorem.words',
+	'lorem.sentence',
+	'lorem.slug',
+	'lorem.sentences',
+	'lorem.paragraph',
+	'lorem.paragraphs',
+	'lorem.text',
+	'lorem.lines',
+	'name.firstName',
+	'name.lastName',
+	'name.findName',
+	'name.jobTitle',
+	'name.prefix',
+	'name.suffix',
+	'name.title',
+	'name.jobDescriptor',
+	'name.jobArea',
+	'name.jobType',
+	'phone.phoneNumber',
+	'phone.phoneNumberFormat',
+	'phone.phoneFormats',
+	'random.number',
+	'random.arrayElement',
+	'random.objectElement',
+	'random.uuid',
+	'random.boolean',
+	'random.word',
+	'random.words',
+	'random.image',
+	'random.locale',
+	'random.alphaNumeric',
+	'system.fileName',
+	'system.commonFileName',
+	'system.mimeType',
+	'system.commonFileType',
+	'system.commonFileExt',
+	'system.fileType',
+	'system.fileExt',
+	'system.directoryPath',
+	'system.filePath',
+	'system.semver'
+];
+
+faker.locale = vscode.workspace.getConfiguration("generateData").get("localisation", "en");
+
+
+const generators = fakerGenerators.map((fg => {
+	var fakerCommand = "{{" + fg + "}}";
+	return {
+		name: fg,
+		func(options) {
+			return faker.fake(fakerCommand);
+		}
+	}
+}));
+
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "generate-data" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let disposable = vscode.commands.registerCommand('extension.generateData', listGenerators);
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+
+function insertData(config) {
+	var editor = vscode.window.activeTextEditor;
+	editor.edit(
+	  edit => editor.selections.forEach(
+		selection => {
+		  edit.delete(selection);
+		  edit.insert(selection.start, generators.find((item) => config.generatorName === item.name).func(config) + ' # ' + faker.locale);
+		}
+	  )
+	);
+  }
+
+function listGenerators() {
+	const options = {
+		matchOnDescription: true,
+		matchOnDetail: false,
+		placeHolder: "Loading Projects (pick one to open)"
+	};
+
+	function onRejectListGenerators(reason) {
+		vscode.commands.executeCommand("setContext", "inDataGeneratorList", false);
+		vscode.window.showInformationMessage(`Error loading generators: ${reason}`);
+	}
+
+	function onResolve(selected) {
+		vscode.commands.executeCommand("setContext", "inDataGeneratorList", false);
+		if (!selected) {
+			return;
+		}
+
+		insertData({
+			count: 1,
+			generatorName: selected
+		});
+
+		vscode.window.showInformationMessage(selected);
+	}
+
+	if (generators.length === 0) {
+		vscode.window.showInformationMessage("No generators available!");
+		return;
+	} else {
+		vscode.commands.executeCommand("setContext", "inDataGeneratorList", true);
+		vscode.window.showQuickPick(generators.map((item) => item.name), options)
+			.then(onResolve, onRejectListGenerators);
+	}
+
+	
+}
 
 // this method is called when your extension is deactivated
 function deactivate() {}
